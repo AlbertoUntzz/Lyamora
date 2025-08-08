9fb5121018c0fc3108c28cb56e98a098925fb45b
AlbertoUntzz <albertolopez13289@gmail.com>
2025-08-08 15:53:42 -0600

    icono ubicacion

--- lyamora/src/components/Navbar.jsx
+++ lyamora/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import lya_mora_logo from '../assets/lya_mora_logo.gif'
 
 export default function Navbar() {
   return (
-    <nav class="sticky top-8 max-w-screen-xl px-10 py-6 mx-auto rounded-xl bg-white/20 backdrop-blur-sm backdrop-saturate-150 border border-white/25 shadow-sm">
+    <nav class="sticky top-8 max-w-screen-xl px-10 py-4 mx-auto rounded-xl bg-white/20 backdrop-blur-sm backdrop-saturate-150 border border-white/25 shadow-sm">
       <div class="container flex flex-wrap items-center justify-between mx-auto text-purple-300">
         <a href="#" class="mr-4 block cursor-pointer text-xl font-bold text-purple-900">
           <img src={lya_mora_logo} alt="Lyamora logo" class= "h-15"></img>
@@ -12,8 +12,9 @@ export default function Navbar() {
         <div class="hidden lg:block">
           <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-8">
             <li class="flex items-center p-1 text-lg gap-x-2 text-purple-900 hover:text-purple-600 transition-colors">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="h-6 w-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
               </svg>
               <a href="#" class="flex items-center">
                 Pages
@@ -56,3 +57,8 @@ export default function Navbar() {
     </nav>
   )
 }
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+</svg>
