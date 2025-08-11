ab91ef62cbc2b7a295ec59d428763194e23033d8
AlbertoUntzz <albertolopez13289@gmail.com>
2025-08-11 15:50:30 -0600

    no se modifico nada

--- lyamora/src/components/Navbar.jsx
+++ lyamora/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
               </svg>
               <a href="#" class="flex items-center">
-                Pages
+                Ubicación
               </a>
             </li>
             <li class="flex items-center p-1 text-lg gap-x-2 text-purple-900 hover:text-purple-600 transition-colors">
--- lyamora/src/main.jsx
+++ lyamora/src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Catalogo from './components/Catalogo.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}/>
-        <Route path='/Catalogo' element={<catalogo/>}/>
-      </Routes>
-    </BrowserRouter>
-    
+    <App/>
   </StrictMode>,
 )
